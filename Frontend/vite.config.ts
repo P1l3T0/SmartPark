4355cd840f78463f91d3431ad7ba79b4e2d9f776
Vasil Ganev <vgan3v@gmail.com>
2026-03-17 22:02:51 +0200

    common(proj): add frontend dependencies

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    define: {
+      "process.env.BACKEND_API_URL": JSON.stringify(env.BACKEND_API_URL),
+    },
+    plugins: [react(), tailwindcss()],
+  };
+});
