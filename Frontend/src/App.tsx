a3ede7c863d05233ad79c42fa048d555f236af22
Vasil Ganev <vgan3v@gmail.com>
2026-03-19 19:36:19 +0200

    feat(navigation): add browser router

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,63 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import NotFound from "./Pages/NotFound";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Home from "./Pages/Home";
+import ParkingSpots from "./Pages/ParkingSpots";
+import Vehicles from "./Pages/Vehicles";
+import Bookings from "./Pages/Bookings";
+import ProtectedRoute from "./Context/Auth/ProtectedRoute";
+import PersistLogin from "./Context/Auth/PersistLogin";
+import Navbar from "./Components/Common/Navbar";
+
 function App() {
-  return <>
-    <h1>SmartPark</h1>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route element={<PersistLogin />}>
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/parking-spots"
+              element={
+                <ProtectedRoute>
+                  <ParkingSpots />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/vehicles"
+              element={
+                <ProtectedRoute>
+                  <Vehicles />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/bookings"
+              element={
+                <ProtectedRoute>
+                  <Bookings />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
