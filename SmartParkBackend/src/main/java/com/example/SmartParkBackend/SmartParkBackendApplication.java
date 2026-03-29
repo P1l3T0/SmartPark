5c2077eaa641f817798a1994c75ce32e5f74a111
Vasil Ganev <vgan3v@gmail.com>
2026-03-29 17:52:58 +0300

    feat(backend): add sample java files

---
+++ SmartParkBackend/src/main/java/com/example/SmartParkBackend/SmartParkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartParkBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkBackendApplication.class, args);
+	}
+
+}
