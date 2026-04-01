4e40b905e2b81a5da48194d0d8cac8699140b46f
Vasil Ganev <vgan3v@gmail.com>
2026-04-01 22:17:32 +0300

    feat(config): add configuration files

---
+++ SmartParkBackend/src/main/java/com/example/SmartParkBackend/config/ApplicationConfiguration.java
@@ -0,0 +1,45 @@
+package com.example.SmartParkBackend.config;
+
+import com.example.SmartParkBackend.Repo.UserRepo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfiguration {
+    private final UserRepo userRepo;
+
+    public ApplicationConfiguration(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+}
