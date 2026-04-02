93e1bfd616809800086077f7faf6b7085c411413
Vasil Ganev <vgan3v@gmail.com>
2026-04-02 22:25:24 +0300

    feat(dto): add getter to base dto

--- SmartParkBackend/src/main/java/com/example/SmartParkBackend/DTO/Response/BaseDTO.java
+++ SmartParkBackend/src/main/java/com/example/SmartParkBackend/DTO/Response/BaseDTO.java
@@ -1,9 +1,11 @@
 package com.example.SmartParkBackend.DTO.Response;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.Date;
 
+@Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
