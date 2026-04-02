aa3036bba9355f47f894e72c6b9ac702251ae8b0
Vasil Ganev <vgan3v@gmail.com>
2026-04-02 20:37:22 +0300

    feat(jwt): add jwt service

--- SmartParkBackend/src/main/java/com/example/SmartParkBackend/config/JwtAuthenticationFilter.java
+++ SmartParkBackend/src/main/java/com/example/SmartParkBackend/config/JwtAuthenticationFilter.java
@@ -1,6 +1,6 @@
 package com.example.SmartParkBackend.config;
 
-import com.example.SmartParkBackend.Service.Impl.JwtServiceImpl;
+import com.example.SmartParkBackend.Service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,12 +22,12 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final HandlerExceptionResolver exceptionResolver;
-    private final JwtServiceImpl jwtService;
+    private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
     public JwtAuthenticationFilter(
             @Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver,
-            JwtServiceImpl jwtService,
+            JwtService jwtService,
             UserDetailsService userDetailsService) {
         this.exceptionResolver = exceptionResolver;
         this.jwtService = jwtService;
