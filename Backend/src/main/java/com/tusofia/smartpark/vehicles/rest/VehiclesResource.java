958c9f445c78a0732df2294e0c1c253df849e6b4
mihaelastoycheva <msdesign.business@gmail.com>
2026-05-13 21:10:55 +0300

    Update REST API type in VehiclesResource

--- Backend/src/main/java/com/tusofia/smartpark/vehicles/rest/VehiclesResource.java
+++ Backend/src/main/java/com/tusofia/smartpark/vehicles/rest/VehiclesResource.java
@@ -6,13 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/vehicles")
@@ -39,7 +33,7 @@ public class VehiclesResource {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/{vehicleId}")
+    @PutMapping("/{vehicleId}")
     public ResponseEntity<Void> updateVehicle(@PathVariable Long vehicleId, @RequestBody VehicleDTO vehicleDTO) {
         LOG.debug("REST request to update current user's Vehicle : {}", vehicleId);
         vehiclesService.updateForCurrentUser(vehicleId, vehicleDTO);
